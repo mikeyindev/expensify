ecbbf26d6ca134f979e37894c8f3749ebb391a77
mikeyindev <mikeyindev@gmail.com>
2018-05-08 20:05:15 -0400

    Set up client-side routing and added routes

--- src/app.js
+++ src/app.js
@@ -1,37 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
 // Need to set up loaders in webpack to load CSS
 import './styles/styles.scss';
 // When importing from node_modules directory, no need to specify path. Normalize.css is used for CSS reset
 import 'normalize.css/normalize.css';
+import AppRouter from './routers/AppRouter'
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is my dashboard component
-  </div>
-);
 
-const AddExpensePage = () => (
-  <div>
-    This is my add expense component
-  </div>
-);
-
-const EditExpensePage = () => <div>This is my edit expense component</div>;
-const HelpPage = () => <div>This is my help component</div>;
-const NotFoundPage = () => <div>404</div>;
-
-const routes = (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(<AppRouter />, document.getElementById("app"));
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import AddExpensePage from '../components/AddExpense';
+import EditExpensePage from '../components/EditExpense';
+import ExpenseDashboardPage from '../components/ExpenseDashboard';
+import HelpPage from '../components/Help';
+import Header from '../components/Header';
+import NotFoundPage from '../components/NotFound';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit/:id" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
