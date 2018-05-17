1a06eaeaf85c29aa90e5dd814041ad226a66059d
mikeyindev <mikeyindev@gmail.com>
2018-05-17 19:10:37 -0400

    Added tests for expense filters

--- src/selectors/filterExpenses.js
+++ src/selectors/filterExpenses.js
@@ -10,22 +10,25 @@ const filterExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
 
     const createdAtMoment = moment(expense.createdAt); 
 
-    // if startDate is undefined (default value), it'll return true and not get
-    // filtered out. Matching all expenses created on and after the day of the
-    // startDate
-    const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+    // If startDate is undefined (default value), a falsy value, then the
+    // ternary expression will return true. The expense won't get filtered out.
+    // Matching all expenses created on or after the day of the filter startDate
+    const startDateMatch = startDate ? createdAtMoment.isSameOrAfter(startDate, "day") : true;
 
-    // Matching all expenses created on and before the day of the endDate
-    const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+    // Matching all expenses created on or before the day of the filter endDate
+    const endDateMatch = endDate ? createdAtMoment.isSameOrBefore(endDate, "day") : true;
 
     // Will only return true if all 3 things matched, if it returns false, that
     // expense would not be included in the returned array
     return textMatch && startDateMatch && endDateMatch;
+
   }).sort((a, b) => {
     if (sortBy === 'date') {
-      return a.createdAt < b.createdAt ? 1 : -1;
+      // Return most recent expenses first
+      return a.createdAt > b.createdAt ? -1 : 1;
     } else if (sortBy === 'amount') {
-      return a.amount > b.amount ? 1 : -1;
+      // Return highest expenses first
+      return a.amount > b.amount ? -1 : 1;
     }
   });
 }
---
+++ src/tests/selectors/filterExpenses.test.js
@@ -0,0 +1,91 @@
+import moment from 'moment';
+import filterExpenses from '../../selectors/filterExpenses';
+
+// Test data
+const expenses = [
+  {
+    id: '1',
+    description: 'Gum',
+    note: '',
+    amount: 195,
+    createdAt: 0
+  },
+  {
+    id: '2',
+    description: 'Rent',
+    note: '',
+    amount: 19500,
+    createdAt: moment(0).subtract(4, 'days').valueOf()
+  },
+  {
+    id: '3',
+    description: 'Credit card',
+    note: '',
+    amount: 4500,
+    createdAt: moment(0).add(4, 'days').valueOf()
+  }
+];
+
+test('should filter by text value', () => {
+  // Should filter out 'Gum'
+  const filters = {
+    text: 'r',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  };
+
+  const result = filterExpenses(expenses, filters);
+  // The result is sorted by date, most recent expenses listed first
+  expect(result).toEqual([ expenses[2], expenses[1] ]);
+});
+
+test('should filter by start date', () => {
+  // Should filter out 'Rent'
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: moment(0),
+    endDate: undefined
+  };
+  const result = filterExpenses(expenses, filters);
+  // The result is sorted by date
+  expect(result).toEqual([ expenses[2], expenses[0] ]);
+});
+
+test('should filter by end date', () => {
+  // Should filter out 'Credit card'
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: moment(0)
+  };
+
+  const result = filterExpenses(expenses, filters);
+  expect(result).toEqual([ expenses[0], expenses[1] ]);
+});
+
+test('should sort by date', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  };
+
+  const result = filterExpenses(expenses, filters);
+  expect(result).toEqual([ expenses[2], expenses[0], expenses[1] ]);
+});
+
+test('should sort by amount', () => {
+  const filters = {
+    text: '',
+    sortBy: 'amount',
+    startDate: undefined,
+    endDate: undefined
+  };
+
+  const result = filterExpenses(expenses, filters);
+  expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
+});
