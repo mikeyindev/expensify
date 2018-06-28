08c3ec2bdd00ab06d37d7b22fc8dd8a06098ab19
mikeyindev <mikeyindev@gmail.com>
2018-06-28 14:21:32 -0500

    Modified login button

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/authActions';
 
-export const LoginPage = (props) => <div className="Login">
-           <div className="Login__box">
-             <h1 className="Login__box__title">Expensify</h1>
-             <p className="Login__box__subtitle">
-               Your expenses on track.
-             </p>
-             <button className="Login__button" onClick={props.startLogin}>
-               <span className="google-logo">
-                <img src="/images/google_logo.svg" height="20px" width="20px" />
-              </span>
-              <span>Login with Google</span>
-             </button>
-           </div>
-         </div>;
+export const LoginPage = (props) => 
+  <div className="Login">
+    <div className="Login__box">
+      <h1 className="Login__box__title">Expensify It</h1>
+      <p className="Login__box__subtitle">
+        Your expenses on track.
+      </p>
+      <button className="Login__button" onClick={props.startLogin}>
+        <span className="google-logo">
+        <img src="/images/google_logo.svg" height="20px" width="20px" />
+      </span>
+      <span>Google Login</span>
+      </button>
+    </div>
+  </div>;
 
 const mapDispatchToProps = (dispatch) => ({
     startLogin: () => dispatch(startLogin())
